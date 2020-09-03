90a88c0e2d731f263c5226727af2c07f91c59ae1
Omar Sabri <oz9.sa7@gmail.com>
2020-09-03 15:41:18 +0300

    🌄 Add images to the grid

--- app/src/main/java/viewholders/PhotosGridViewHolder.kt
+++ app/src/main/java/viewholders/PhotosGridViewHolder.kt
@@ -2,6 +2,8 @@ package viewholders
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.omzer.photosviewer.R
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.photo_card.view.*
 import models.PhotoModel
 
@@ -10,5 +12,12 @@ class PhotosGridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun setData(photoModel: PhotoModel) {
         itemView.author.text = photoModel.author
+        val img = itemView.image
+        Picasso.get()
+            .load(photoModel.downloadUrl)
+            .centerCrop()
+            .resize(500, 500)
+            .placeholder(R.color.grey_purple)
+            .into(img)
     }
 }

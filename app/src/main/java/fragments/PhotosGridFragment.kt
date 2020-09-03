3588a089dfebac4d918872c0b1e64aa12fe04f07
Omar Sabri <oz9.sa7@gmail.com>
2020-09-03 14:21:06 +0300

    🔽 Get the data from the API into the fragment

--- app/src/main/java/fragments/PhotosGridFragment.kt
+++ app/src/main/java/fragments/PhotosGridFragment.kt
@@ -5,12 +5,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
 import com.omzer.photosviewer.R
+import kotlinx.android.synthetic.main.photos_grid_fragment.*
+import models.PhotoModel
+import viewmodels.PhotosGridViewModel
 
 class PhotosGridFragment : Fragment() {
 
+    private lateinit var viewModel: PhotosGridViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, b: Bundle?): View? {
+        viewModel = ViewModelProvider(this).get(PhotosGridViewModel::class.java)
+        observe()
         return inflater.inflate(R.layout.photos_grid_fragment, container, false)
+    }
+
+    private fun observe() {
+        viewModel.photosRequestObserver.observe(this, Observer { onPhotosReceived(it) })
+    }
+
+    private fun onPhotosReceived(photos: List<PhotoModel>) {
+        loadingIndicator.visibility = View.GONE
+        if (photos.isEmpty()) {
+            showSnackbar(getString(R.string.connection_issue))
+            return
+        }
+
+    }
+
+    private fun showSnackbar(message: String) {
+        Snackbar.make(rootLayout, message, Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewModel.requestPhotos()
     }
 
 }
